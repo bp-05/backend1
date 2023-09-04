8026740a46afea42f1fc53b518bac582a7faec1c
bp05 <bastian.parada05@inacapmail.cl>
2023-09-04 16:21:33 -0300

    primera app con dos vistas, rama1 terminada

---
+++ app1/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def vista1(request):
+    html="""
+    <h1 style='color:blue'>Bienvenido!</h1>
+    <a href="/vista2">Ver vista 2</a>
+    
+    """
+    return HttpResponse(html)
+
+
+def vista2(request):
+    html="""
+    <h1 style='color:red'>Bienvenido a la vista 2</h1>
+    <a href="http://127.0.0.1:8000/">Volver</a>
+    
+    """
+    return HttpResponse(html)    
